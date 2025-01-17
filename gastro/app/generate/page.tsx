c493837f0f1703a959c140f47e04f192c465ccc7
SusheelThapa <susheelthapa90@gmail.com>
2025-01-18 01:13:54 +0545

    feat: deployment info notice

--- gastro/app/generate/page.tsx
+++ gastro/app/generate/page.tsx
@@ -1,20 +1,30 @@
 "use client";
 
-import React from "react";
-
+import React, { useState } from "react";
 import GastroWrapper from "@/components/gastro-wrapper";
 import { GastroProvider } from "@/lib/gastro-provider";
-
 import { CopilotKit } from "@copilotkit/react-core";
 import "@copilotkit/react-ui/styles.css";
+import InfoModal from "@/components/info-modal";
 
 const GastroPage: React.FC = () => {
+  const [showInfo, setShowInfo] = useState(true);
+
+  const handleCloseButton = () => {
+    setShowInfo(false);
+  };
+
   return (
-    <CopilotKit runtimeUrl="/api/copilotkit" agent="gastro_agent">
-      <GastroProvider>
-        <GastroWrapper />
-      </GastroProvider>
-    </CopilotKit>
+    <>
+      {showInfo && <InfoModal handleCloseButton={handleCloseButton} />}
+      <div>
+        <CopilotKit runtimeUrl="/api/copilotkit" agent="gastro_agent">
+          <GastroProvider>
+            <GastroWrapper />
+          </GastroProvider>
+        </CopilotKit>
+      </div>
+    </>
   );
 };
 
